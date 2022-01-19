06f960ec8818df272d5b8af162a22bdcd0d7b596
Xiaopeng Lu <vincentlu073@gmail.com>
2022-01-19 13:48:09 +0800

    Update user-add.sh

--- commands/user-add.sh
+++ commands/user-add.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 for user in `more users.txt`
 do
-echo "$user"
-useradd $user
-echo "password-you-like-$user" | passwd --stdin "$user"
-chage -d 0 $user
-gpasswd -a $user wheel
+if id "$user" &>/dev/null; then
+  echo "$user found"
+else
+  echo "creating $user"
+  mkdir /home/$user
+  adduser --home /home/$user $user
+  chown -R $user:$user /home/$user
+  echo "passyoulike@@" | passwd --stdin "$user"
+  chage -d 0 $user
+  echo "give $user root access"
+  gpasswd -a $user wheel
+fi
 done
